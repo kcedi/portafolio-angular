3dfc7c34f05a685b64528b412a0c4fd7f8d42df0
kcedi <li.kcedillo@gmail.com>
2021-03-24 22:14:37 -0600

    Fin sección 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,12 @@
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twiter?: string;
+  instagram?: string;
+  tumbler?: string;
+  equipo_trabajo?: any[];
+}
